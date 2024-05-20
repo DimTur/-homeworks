af346eff6f43a6f9054a45657b92808ebb58ddce
Dmitrii Turetskikh <tureckihdmitriq@gmail.com>
2024-05-20 11:56:51 +0300

    fix some bugs

---
+++ 1.homework_1/2.task#2/cmd_tool/create_tool.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func createFile(fileName string, overwrite bool) {
+	if _, err := os.Stat(fileName); err == nil {
+		if !overwrite {
+			fmt.Printf("File %s already exists and overwrite is not enabled.\n", fileName)
+			return
+		}
+	}
+
+	file, err := os.Create(fileName)
+	if err != nil {
+		fmt.Println("File creating error:", err)
+		return
+	}
+	defer file.Close()
+	fmt.Printf("File %s successfully created.\n", fileName)
+}
---
+++ 1.homework_1/2.task#2/cmd_tool/file_tool.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	usageFileToolCmd := flag.NewFlagSet("help", flag.ExitOnError)
+	createFileCmd := flag.NewFlagSet("create", flag.ExitOnError)
+	// readFileCmd := flag.NewFlagSet("read", flag.ExitOnError)
+	// deleteFileCmd := flag.NewFlagSet("delete", flag.ExitOnError)
+
+	listCommands := usageFileToolCmd.String("help", "", "What commadns can we use")
+
+	createFileName := createFileCmd.String("filename", "", "File's name to create")
+	createFileNameOverwrite := createFileCmd.Bool("overwrite", false, "Overwrite file if it already exists")
+
+	if len(os.Args) < 2 {
+		fmt.Println("Invalid command.")
+		fmt.Println("Use 'help' command.")
+	}
+
+	switch os.Args[1] {
+	case "help":
+		usageFileToolCmd.Parse(os.Args[1:])
+		fmt.Println("help:", *listCommands)
+		fmt.Println("Usage:")
+		fmt.Println("	If you need to create new file:")
+		fmt.Println("		create --filename=<filename> : Create a new file")
+		fmt.Println("		create --filename=<filename> --overwrite: Create a new file and overwrite fith the same name")
+	case "create":
+		createFileCmd.Parse(os.Args[2:])
+		fmt.Println("Use help command")
+		fmt.Println("filename:", *createFileName)
+		fmt.Println("overwrite:", *createFileNameOverwrite)
+		if *createFileName == "" {
+			fmt.Println("Please specify a filename to create")
+			createFileCmd.PrintDefaults()
+			os.Exit(1)
+		}
+		createFile(*createFileName, *createFileNameOverwrite)
+	default:
+		fmt.Println("Expected current command")
+	}
+}
