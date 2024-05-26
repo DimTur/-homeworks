baa24123ad79445ee4b40fc74093a35461c7face
Dmitrii Turetskikh <tureckihdmitriq@gmail.com>
2024-05-26 10:38:06 +0300

    feat(2.homework_2): create task #4

---
+++ 2.homework_2/4.task#4/db_helper.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+func summaryTable(database Database) {
+	studentsData := make(map[int]Students)
+	objectsData := make(map[int]Objects)
+	subjectsGrade := make(map[string]map[int][]int)
+
+	for _, student := range database.Students {
+		studentsData[student.Id] = student
+	}
+
+	for _, object := range database.Objects {
+		objectsData[object.Id] = object
+		subjectsGrade[object.Name] = make(map[int][]int)
+	}
+
+	for _, result := range database.Results {
+		student := studentsData[result.Student_id]
+		object := objectsData[result.Object_id]
+		grades := subjectsGrade[object.Name][student.Grade]
+		grades = append(grades, result.Result)
+		subjectsGrade[object.Name][student.Grade] = grades
+	}
+
+	for subject, subjectGrade := range subjectsGrade {
+		fmt.Printf("________________\n")
+		fmt.Printf("%-13s | Mean\n", subject)
+		fmt.Printf("________________\n")
+
+		var totalSum int
+		var totalCount int
+
+		for grade, grades := range subjectGrade {
+			sum := 0
+			for _, gradeValue := range grades {
+				sum += gradeValue
+			}
+			mean := float64(sum) / float64(len(grades))
+			totalSum += sum
+			totalCount += len(grades)
+
+			fmt.Printf("%-7d grade | %.1f\n", grade, mean)
+		}
+
+		totalMean := float64(totalSum) / float64(totalCount)
+		fmt.Printf("________________\n")
+		fmt.Printf("mean          | %.1f\n", totalMean)
+		fmt.Printf("________________\n")
+	}
+}
