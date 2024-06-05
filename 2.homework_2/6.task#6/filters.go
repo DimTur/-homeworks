ca6ba38087bfc61fceae4458b39dd0fc9d248ef8
Dmitrii Turetskikh <tureckihdmitriq@gmail.com>
2024-06-05 16:14:06 +0300

    fix(HW_2): 6th task fixed

--- 2.homework_2/6.task#6/filters.go
+++ 2.homework_2/6.task#6/filters.go
@@ -7,23 +7,18 @@ type Number interface {
 }
 
 func Filter[T any](subject string, subjectsGrade map[string]map[int][]T) map[int][]T {
-	filtered := make(map[int][]T)
-
-	if grades, ok := subjectsGrade[subject]; ok {
-		for grade, gradeValues := range grades {
-			filtered[grade] = gradeValues
-		}
-	} else {
+	filtered, ok := subjectsGrade[subject]
+	if !ok {
 		fmt.Printf("Subject %s not found\n", subject)
+		return make(map[int][]T)
 	}
-
 	return filtered
 }
 
-func Mean[T Number](grades []T) float64 {
-	var sum T
+func Mean[IT, OT Number](grades []IT) OT {
+	var sum IT
 	for _, grade := range grades {
 		sum += grade
 	}
-	return float64(sum) / float64(len(grades))
+	return OT(float64(sum) / float64(len(grades)))
 }
--- 2.homework_2/6.task#6/main.go
+++ 2.homework_2/6.task#6/main.go
@@ -4,11 +4,11 @@ func main() {
 	dbUrl := "../dz3.json"
 	dataBase := getDb(dbUrl)
 
-	subject1 := "Math"
-	subject2 := "Biology"
-	subject3 := "Geography"
+	subjects := []string{"Math", "Biology", "Geography"}
 
-	summaryTable(dataBase, subject1)
-	summaryTable(dataBase, subject2)
-	summaryTable(dataBase, subject3)
+	for _, sub := range subjects {
+		summaryTable(dataBase, sub, func(subject string, filteredGrades map[int][]int) {
+			printSummary(subject, filteredGrades)
+		})
+	}
 }
